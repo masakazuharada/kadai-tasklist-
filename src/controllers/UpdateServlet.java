3c06f8ad046014f736ab15d7bbfa696c8431aa92
masakazu.harada <masakazu.harada@growship.com>
2019-09-17 18:33:05 +0900

    Add validation

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.tasks;
+import models.validators.TaskValidator;
 import utils.DBUtil;
 
 /**
@@ -46,6 +49,32 @@ public class UpdateServlet extends HttpServlet {
             m.setUpdated_at(currentTime);       // 更新日時のみ上書き
             String content = request.getParameter("content");
             m.setContent(content);
+         // バリデーションを実行してエラーがあったら編集画面のフォームに戻る
+            List<String> errors = TaskValidator.validate(m);
+            if(errors.size() > 0) {
+                em.close();
+
+                // フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("tasks", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
+                rd.forward(request, response);
+            } else {
+                // データベースを更新
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "更新が完了しました。");
+                em.close();
+
+                // セッションスコープ上の不要になったデータを削除
+                request.getSession().removeAttribute("tasks_id");
+
+                // indexページへリダイレクト
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
+
 
 
             // データベースを更新
